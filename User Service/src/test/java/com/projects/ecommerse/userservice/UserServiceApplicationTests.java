7ac7c9ba3303580054e2b12da6712f1c264a91e4
Sanket <sanketpadavale12902@gmail.com>
2026-08-18 17:06:29 +0530

    first commit

---
+++ User Service/src/test/java/com/projects/ecommerse/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.ecommerse.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
